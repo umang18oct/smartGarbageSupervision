975e89f2a687821fbe7002ef252b03f83e9659de
Umang Chaudhary <umang18oct@gmail.com>
2016-12-05 14:44:43 +0530

    Arduino file

---
+++ SmartBin.ino
@@ -0,0 +1,101 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <ESP8266HTTPClient.h>
+
+#define USE_SERIAL Serial
+
+#define TRIGGER 5
+#define ECHO    4
+
+ESP8266WiFiMulti WiFiMulti;
+long maximum = 0;
+
+void setup() {
+
+    USE_SERIAL.begin(115200);
+   // USE_SERIAL.setDebugOutput(true);
+   
+  pinMode(TRIGGER, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+    for(uint8_t t = 4; t > 0; t--) {
+        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+        USE_SERIAL.flush();
+        delay(1000);
+    }
+
+    WiFiMulti.addAP("bye", "helloworld");
+  for(int i=0;i<5;i++){
+    long distance = getReading();
+    maximum += distance; 
+    delay(500);
+  }
+  maximum /= 5;
+}
+int val = 0;
+
+int getReading(){
+  long duration, distance;
+  digitalWrite(TRIGGER, LOW);  
+  delayMicroseconds(2); 
+  
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10); 
+  
+  digitalWrite(TRIGGER, LOW);
+  duration = pulseIn(ECHO, HIGH);
+  Serial.println(duration);
+  distance = (duration/2) / 29.1;
+  
+  Serial.print(distance);
+  Serial.println("Centimeter:");
+
+  return distance;
+}
+
+void loop() {
+    // wait for WiFi connection
+    if((WiFiMulti.run() == WL_CONNECTED)) {
+
+        HTTPClient http;
+
+        USE_SERIAL.print("[HTTP] begin...\n");
+        // configure traged server and url
+        //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
+        float perc = (maximum-getReading())*100.0/maximum;
+        Serial.print("Percentage filled = ");
+        Serial.println(perc);
+        http.begin("http://nodeapp-mdakram28.c9.io/update?id=2&val="+String((int)perc)); //HTTP
+        val += 10;
+        if(val>=100)val=0;
+        USE_SERIAL.print("[HTTP] GET...\n");
+        // start connection and send HTTP header
+        int httpCode = http.GET();
+
+        // httpCode will be negative on error
+        if(httpCode > 0) {
+            // HTTP header has been send and Server response header has been handled
+            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+
+            // file found at server
+            if(httpCode == HTTP_CODE_OK) {
+                String payload = http.getString();
+                USE_SERIAL.println(payload);
+            }
+        } else {
+            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+
+        http.end();
+    }
+
+    delay(1000);
+}
+
